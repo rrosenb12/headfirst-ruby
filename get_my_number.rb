cab4af8ee850f86ce9bf01dd7d5847e621f68e38
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2022-05-13 21:07:42 -0400

    conditionals for player guess

--- get_my_number.rb
+++ get_my_number.rb
@@ -20,8 +20,12 @@ answer = rand(100) + 1
 # Keep track of how many guesses the user has taken; increment by 1 with every guess
 number_of_guesses = 0
 
-# The player only gets 10 guesses; keep track of how many guesses the user has left using the number of guesses they already took
-remaining_guesses = 10 - number_of_guesses
+# Keep track of whether or not the player guessed the number
+
+guessed_it = false
+
+# The player only gets 10 guesses; keep track of how many guesses the user has left using the number of guesses they already took (this is being done in string interpolation currently):
+## remaining_guesses = 10 - number_of_guesses
 
 # The following all return the same thing:
 ## puts remaining_guesses.to_s + " guesses left."
@@ -36,4 +40,18 @@ print "Make a guess: "
 ## No need to call chomp here because to_i will remove any non-numeric character after the number
 guess = gets.to_i 
 
-puts guess < answer
+# Compare the guess to the answer and print the appropriate message
+if guess < answer
+    puts "Oops, your guess was LOWER than the answer"
+elsif guess > answer
+    puts "Oops, your guess was HIGHER than the answer"
+elsif guess == answer
+    puts "Good job, #{name}!"
+    puts "You guessed my number in #{number_of_guesses} guesses!"
+    guessed_it = true
+end
+
+# If the player runs out of turns, tell them what the number was
+if not guessed_it
+    puts "Sorry. You didn't get my number. It was #{answer}."
+end
