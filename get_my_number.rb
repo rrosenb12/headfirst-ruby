ef6985d11b23903f7b6d3da2b4db5c800728ca06
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2022-05-13 21:15:58 -0400

    unless statement

--- get_my_number.rb
+++ get_my_number.rb
@@ -52,6 +52,11 @@ elsif guess == answer
 end
 
 # If the player runs out of turns, tell them what the number was
-if not guessed_it
+## Can use the below, but can use the un-commented out code for easier reading
+=begin if not guessed_it
     puts "Sorry. You didn't get my number. It was #{answer}."
+end
+=end
+unless guessed_it
+    puts "Sorry. You didn't get my number. It was #{answer}"
 end
